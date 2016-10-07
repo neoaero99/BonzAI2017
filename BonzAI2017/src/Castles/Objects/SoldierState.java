181062705759a26526a19ef93c343b3f01d9cd36
Lunasol <dcmohrha@mtu.edu>
2016-10-07 18:00:20 -0400

    Added the enumeration

---
+++ BonzAI2017/src/Castles/Objects/SoldierState.java
@@ -0,0 +1,16 @@
+package Castles.Objects;
+
+/**
+ * SoldierState Enumeration
+ * 
+ * @author David Mohrhardt
+ * 
+ * Description:
+ * This is an enumeration that is intended for indicating the state of a
+ * soldier at any given time.
+ *
+ */
+
+public enum SoldierState {
+	STANDBY, MOVING, CONFLICT, SIEGING, DEFENDING
+}
