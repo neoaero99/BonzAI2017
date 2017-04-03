717bcf1fa7d7fb5ded90279f34aa3bf433335c22
DaneJensen <firstlegoboy@comcast.net>
2017-04-03 16:24:47 -0400

    made changes so that the brackets are now automagically genorated

--- BonzAI2017/src/DaneJensenBrackets/Bracket.java
+++ BonzAI2017/src/DaneJensenBrackets/Bracket.java
@@ -194,31 +194,158 @@ public class Bracket {
 		for(int i=0; i < ais.size(); i++){
 			System.out.println("" + i + ": " + ais.get((int)avgScores[i][1]).name());
 		}
-		/*
+		
 		//generate the bracket
+		
+		/* 
+		 * determines how large the bracket needs to be
+		 * i.e, if there are 41 teams, there needs to be room
+		 * for 64 teams, or 32 teams with several teams getting by
+		 * rounds
+		 * output format should look like:
+		 * Round 1
+		 * 
+		 * Match 1:       
+		 * Team[0] (seed)
+		 * Team[1] (seed)
+		 * 
+		 * Match 2:
+		 * Team[0] (seed)
+		 * Team[1] (seed)
+		 * 
+		 * How to handle numTeams not being a power of 2:
+		 * Have the lowest n teams (where .5n is the difference between
+		 * numTeams and the greatest power of 2 that is < numTeams)
+		 * play for the right to play in the bracket
+		 * i.e. I have to multiply n by 2 to get the number of teams that
+		 * 		play for the right to compete
+		 * 
+		 * Math to determine bracket size:
+		 * I have n teams (i'll use 41 for comment purposes)
+		 * That means that the greatest power of 2 < 41 is 32
+		 * with 32 teams in the bracket, the top 16 will be safe
+		 * that leaves 25 teams
+		 * the greatest power of 2 < 25 is 16, that means that the
+		 * next 7 teams are safe.
+		 * the last 18 play for the right to move on
+		 * 
+		 * Bracket will be determined by the following:
+		 * 1 v L9
+		 * 2 v L8
+		 * 3 v L7
+		 * 4 v L6
+		 * 5 v L5
+		 * 6 v L4
+		 * 7 v L3
+		 * 8 v L2
+		 * 9 v L1
+		 * 10th seed v 23rd
+		 * 11th seed v 22nd seed
+		 * 12th seed v 21st seed
+		 * 13th seed v 20th seed
+		 * 14th seed v 19th seed
+		 * 15th seed v 18th seed
+		 * 16th seed v 17th seed
+		 */
 		int n = 2;
-		while(n < (ais.size()/2)+1){
+		while(n < ais.size()/2){
 			n*=2;
 		}
+		System.out.println();
+		System.out.println();
+		System.out.println(n);
+		int disc = (ais.size() - n)*2;
 		
-		bonzai.Jar[][] bracket = new bonzai.Jar[n][2];
-		int start = ais.size()/2;
-		int m = 0;
-		while(start+m < ais.size()){
-			bracket[m][0] = ais.get((int)avgScores[start+m][1]);
-			bracket[m][1] = ais.get((int)avgScores[start-m][1]);
+		String[][] lb = new String[n/4][2];
+		String[][] rb = new String[n/4][2];
+		int[] pos = new int[n];
+		int[] indexs = new int[4];
+		//with the example of 41 teams
+		indexs[0] = 0;//top left				0
+		indexs[1] = n/2;//top right				16
+		indexs[2] = (n/2)-1;//bottom left		15
+		indexs[3] = (n)-1;//bottom right		31
+		int j = 0;
+		int sign = 1;
+		
+		System.out.println("Generating Brackets");
+		//ensures that 1 and 2 are on opposite sides of the bracket
+		//and that the 2 sides of the brackets are roughly as difficult
+		for(int i = 0; i < n; i++){
+			pos[indexs[j]] = i;
+			if(j <= 2){
+				indexs[j]+=2;
+			}else{
+				indexs[j]-=2;
+			}
+			j+=sign;
+			if(j == 4){
+				j = 3;
+				sign = -1;
+			}
+			if(j == -1){
+				sign = 1;
+				j = 0;
+			}
 		}
 		
-		//odd number of teams
-		if(ais.size()%2 == 1){
-			
-			
-		//even number of teams
-		}else{
-			
-		}*/
-		
-		
+		//matches are in sets of 2, so pos[0] v pos[1], pos[2] v pos[3]
+		//now set up the brackets (L + i will be substituted for names
+		//in the event that a pos is a seed facing elemination
+		for(int i = 0; i < n/2; i+=2){
+			if(pos[i] > disc){
+				lb[i/2][0] = "L" + (pos[i] - disc);
+			}else{
+				lb[i/2][0] = ais.get(pos[i]).name();
+			}
+			if(pos[i+1] > disc){
+				lb[i/2][1] = "L" + (pos[i] - disc);
+			}else{
+				lb[i/2][1] = ais.get(pos[i+1]).name();
+			}
+			if(pos[i + n/2] > disc){
+				rb[i/2][0] = "L" + (pos[i] - disc);
+			}else{
+				rb[i/2][0] = ais.get(pos[i + n/2]).name();
+			}
+			if(pos[i + 1 + n/2] > disc){
+				rb[i/2][1] = "L" + (pos[i] - disc);
+			}else{
+				rb[i/2][1] = ais.get(pos[i + n/2 + 1]).name();
+			}
+		}
+		System.out.println("Brackets Generated");
+		//print the brackets to an output file
+		//to be read in by the UI for bonzai brackets
+		System.out.println("Outputing Brackets");
+		try {
+			FileWriter fs = new FileWriter(outFile);
+			BufferedWriter writer = new BufferedWriter(fs);
+			writer.write("Left Bracket:");
+			writer.newLine();
+			for(int i = 0; i < lb.length; i++){
+				writer.write("Match " + i);
+				writer.newLine();
+				writer.write(lb[i][0]);
+				writer.newLine();
+				writer.write(lb[i][1]);
+				writer.newLine();
+			}
+			writer.write("Right Bracket:");
+			writer.newLine();
+			for(int i = 0; i < rb.length; i++){
+				writer.write("Match " + i);
+				writer.newLine();
+				writer.write(rb[i][0]);
+				writer.newLine();
+				writer.write(rb[i][1]);
+				writer.newLine();
+			}
+			writer.close();
+		} catch(Exception e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+		System.out.println("Brackets Outputed");
 		
 	}
 	
