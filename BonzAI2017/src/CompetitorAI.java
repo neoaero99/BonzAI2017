aec55e05161ba774cce8b8324aec2a14ed1b1bad
Joshua <jbhooker@mtu.edu>
2017-04-07 10:26:22 -0400

    Readded CompetitorAI

---
+++ BonzAI2017/src/CompetitorAI.java
@@ -0,0 +1,34 @@
+import java.util.List;
+
+import Castles.api.*;
+import bonzai.*;
+
+@Agent(name ="Test")
+public class CompetitorAI extends AI {
+	
+	@Override
+	public Action action(Turn turn) {
+		
+		Team t = turn.getMyTeam();
+		MoveAction m = new MoveAction();
+		List<SoldierData> soldiers = turn.getSoldiersControlledBy( t.getColor() );
+		
+		if (soldiers.size() > 0) {
+			
+			SoldierData s = soldiers.get(0);
+			
+			if (s.path != null && s.path.size() > 1) {
+				
+				if (s.size > PositionData.BASE_DV) {
+					
+					PositionData enemyBase = turn.getBaseFor(t);
+				
+					m.addMove(s.sIdx, s.size, s.posID, enemyBase.ID);
+					
+				}
+			}
+		}
+		
+		return ( m.numOfActions() > 0 ) ? m : new ShoutAction("No soldiers moved!");
+	}
+}
