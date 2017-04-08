ef2614bbceccdbb999f9bd0ebd3d6e1af6a16d3c
DaneJensen <firstlegoboy@comcast.net>
2017-04-08 10:53:17 -0400

    brackets

--- BonzAI2017/src/DaneJensenBrackets/BracketDisplay.java
+++ BonzAI2017/src/DaneJensenBrackets/BracketDisplay.java
@@ -7,16 +7,29 @@ import javax.swing.*;
 import bonzai.*;
 import Castles.*;
 import Castles.util.*;
-
 import java.io.*;
-
-public class BracketDisplay extends Component implements MouseListener{
-
+//i didn't want warnings for importing things that i was never using
+@SuppressWarnings("unused")
+public class BracketDisplay extends Component implements MouseListener,Runnable{
+	/*
+	 * TODO
+	 * 1. print pre-matches (or empty matches because I like consistency)
+	 * 2. fill in the rest of the brackets with match dependencies
+	 * 3. test test test test test test test test test test test
+	 */
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private ArrayList<Match> matches = new ArrayList<>();
+	private ArrayList<Match> preMatches = new ArrayList<>();
+	private int n = 0; //the number of matches
+	private int x=0,y=0;
+	public boolean end = false;
+	private int frame = 0;
+	private int logn = 0;
+	private int matchN = 0;
+	private Font highlightF = new Font("Times New Roman", Font.BOLD, 40);
 
 	public static void main(String[] args){
 		if(args.length != 2){
@@ -31,11 +44,20 @@ public class BracketDisplay extends Component implements MouseListener{
 		ArrayList<bonzai.Jar> ais = (ArrayList<bonzai.Jar>)b.getAIs(aidir);
 		b.getMatches(ais, bfile);
 		f.setTitle("Bonzai 2017 Brackets");
+		f.setSize(1400, 900);
 		f.setExtendedState(JFrame.MAXIMIZED_BOTH);
 		p.add(b, BorderLayout.CENTER);//makes my bracket display take up the entire screen
+		p.addMouseListener(b);
 		f.add(p);
 		//should be the LAST line of any UI code
 		f.setVisible(true);
+		b.run();
+	}
+	
+	public BracketDisplay(){
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		this.x = (int)screenSize.getWidth();
+		this.y = (int)screenSize.getHeight();
 	}
 	
 	public java.util.List<bonzai.Jar> getAIs(String aiDir){
@@ -61,17 +83,76 @@ public class BracketDisplay extends Component implements MouseListener{
 		try {
 			BufferedReader r = new BufferedReader(new FileReader(bfile));
 			boolean hasNext = true;
+			int qual = 0;
+			n = 0;//the number of matches
 			while(hasNext){
 				String line = r.readLine();
-				System.out.println(line);
+				//System.out.println(line);
 				if(line.contains("Bracket")){
 					continue;
 				}else if(line.equals("Not Placed")){
-					
+					//System.out.println("Making " + qual + " Extra Match(es)");
+					//add extra matches (should add the rest of the ais)
+					for(int i = 0; i < qual; i++){
+						String t1 = r.readLine();
+						String t2 = r.readLine();
+						//System.out.println(t1 + "\n" + t2);
+						bonzai.Jar[] teams = new bonzai.Jar[2];
+						for(int j = 0; j < ais.size(); j++){
+							if(ais.get(j).name().equals(t1)){
+								teams[0] = ais.get(j);
+							}else if(ais.get(j).name().equals(t2)){
+								teams[1] = ais.get(j);
+							}else{
+								//not nessisary to tell the loop to continue
+								//but i thought it looked more readable
+								continue;
+							}
+						}
+						preMatches.add(new Match(teams[0], teams[1]));
+					}
+					break;
 				}else{
+					n++;
 					String t1 = r.readLine();
 					String t2 = r.readLine();
-					System.out.println(t1 + "\n" + t2);
+					boolean sb = false;
+					if(t1.contains("L")){
+						try{
+							String[] parts = t1.split("L");
+							if(parts[0].length() == 0){//string starts with L
+								//surrounded by try/catch so that if someone just
+								//named their ai L* it wouldn't cause the
+								//program to crash
+								int temp = Integer.parseInt(parts[1]);
+								sb = true;
+								String tempS = t1;
+								t1 = t2;
+								t2 = tempS;
+								qual++;
+							}
+						}catch(Exception e){
+							
+						}
+					}
+					
+					if(!sb && t2.contains("L")){
+						try{
+							String[] parts = t2.split("L");
+							if(parts[0].length() == 0){//string starts with L
+								//surrounded by try/catch so that if someone just
+								//named their ai L* it wouldn't cause the
+								//program to crash
+								int temp = Integer.parseInt(parts[1]);
+								sb = true;
+								qual++;
+							}
+						}catch(Exception e){
+							
+						}
+					}
+					
+					//System.out.println(t1 + "\n" + t2);
 					bonzai.Jar[] teams = new bonzai.Jar[2];
 					for(int i = 0; i < ais.size(); i++){
 						if(ais.get(i).name().equals(t1)){
@@ -84,7 +165,12 @@ public class BracketDisplay extends Component implements MouseListener{
 							continue;
 						}
 					}
-					matches.add(new Match(teams[0], teams[1]));
+					if(sb){
+						matches.add(new Match(teams[0]));
+					}else{
+						matches.add(new Match(teams[0], teams[1]));
+					}
+					
 				}
 				
 			}
@@ -94,13 +180,154 @@ public class BracketDisplay extends Component implements MouseListener{
 		}
 		this.matches = matches;//make sure that the rest of the program has
 							   //access to the matches list
+		for(int i =0,j=0; i < n; i++){
+			if(!matches.get(i).hasTwoTeams()){
+				matches.get(i).setMatchDependancy(preMatches.get(j++));
+			}
+		}
+		logn = 0;
+		while(Math.pow(2, logn) < n){
+			logn++;
+		}
+		if(n < ais.size()){
+			logn++;
+		}
+		
+		//fill da brackets
+		int index = 0;
+		for(int i = logn-1; i > 0; i--){
+			int num = (int)Math.pow(2, i);
+			for(int j = 0; j < num; j+=2){
+				matches.add(new Match(matches.get(j+index),matches.get(j+1+index)));
+			}
+			index += num;
+		}
+		
 		return matches;
 	}
 	
 	@Override
 	public void paint(Graphics g){
+		//make animator to scroll brackets
 		super.paint(g);
-		//matches.get(0).drawMatch(g, 0, 0, 100, 50);
+		
+		int xoffset = 30;
+		int xpadding = 50;
+		int ypadding = 10;
+		int matchxsize = 120;
+		int matchysize = 40;
+		int yOffFrame = -1;
+		if((frame*yOffFrame+y)<0){
+			frame = 0;
+		}
+		//n = 32;
+		if(n * (matchysize + ypadding) >= y){
+			y = n * (matchysize + ypadding);
+		}else{
+			yOffFrame = 0;
+		}
+		/*
+		logn = 5;
+		for(int j = 0; j <= logn; j++){
+			int i = (int)Math.pow(2,j);
+			int numB = i*2;
+			int xs = x - (xpadding*(j)) - xoffset - matchxsize*(j+1);
+				
+				for(;i>0;i--){
+					int ys = (y/numB) * (2*i-1) + (-matchysize/2 + yOffFrame * frame);
+					Match.drawEmptyMatch(g,xs, ys, matchxsize, matchysize);
+				}
+		}*/
+		
+		//print brackets
+		int m = 1;
+		for(int j = 0; j <logn; j++){
+			int i = (int)Math.pow(2,j);
+			int numB = i*2;
+			int xs = x-(xpadding*(j))-xoffset-matchxsize*(j+1);
+			for(; i > 0; i--){
+				Match[] dep = matches.get(matches.size()-m).getMatchDependencies();
+				if(dep == null) break;
+				int[] deps = new int[2];
+				if(dep[0]!=null)deps[0]++;
+				if(dep[1]!=null)deps[1]++;
+				int ys = (y/numB) * (2*i-1) + yOffFrame*frame + 5;
+				int inter = (y/numB)/2;
+				if(deps[0] == 1 || deps[1] == 1){
+					g.drawLine(xs, ys, xs-xpadding/2, ys);
+				}
+				if(deps[1] == 1){
+					g.drawLine(xs-xpadding/2, ys, xs-xpadding/2, ys + inter);
+					g.drawLine(xs-xpadding/2, ys + inter, xs-xpadding, ys + inter);
+				}
+				if(deps[0] == 1){
+					g.drawLine(xs-xpadding/2, ys, xs-xpadding/2, ys - inter);
+					g.drawLine(xs-xpadding/2, ys - inter, xs-xpadding, ys - inter);
+				}
+				m++;
+			}
+		}
+		int index = matches.size();
+		//yay matches
+		for(int j = 0; j <= logn; j++){
+			int i = (int)Math.pow(2,j);
+			int numB = i*2;
+			int xs = x - (xpadding*(j)) - xoffset - matchxsize*(j+1);
+			if(i == n){
+				for(;i>0;i--){
+					int ys = (y/numB) * (2*i-1) + (-matchysize/2 + yOffFrame * frame);
+					matches.get(i-1).drawMatch(g,xs, ys, matchxsize, matchysize);
+					Match[] temp = matches.get(i-1).getMatchDependencies();
+					if(temp[0] != null){
+						temp[0].drawMatch(g, xs-xpadding-matchxsize , ys - y/(numB*2), matchxsize, matchysize);
+					}
+						
+					if(temp[1] != null){
+						temp[1].drawMatch(g, xs-xpadding-matchxsize , ys + y/(numB*2), matchxsize, matchysize);
+					}
+					
+				}
+				break;
+			}else{
+				for(;i>0;i--){
+					int ys = (y/numB) * (2*i-1) + (-matchysize/2 + yOffFrame * frame);
+					matches.get(index - i).drawMatch(g,xs, ys, matchxsize, matchysize);
+				}
+				index -= i;
+			}
+		}
+		
+		//creates a separate scope with no logic attached
+		{
+			//print current match
+			int xs = x - (xpadding*(logn+1)) - xoffset - matchxsize*(logn+2);
+			//print next match
+			//g.drawLine(xs, 0, xs, y);
+			xs = 0 + xoffset;
+			int xsize = 400;
+			int ysize = 160;
+			int height = Toolkit.getDefaultToolkit().getScreenSize().height;
+			int padding = height/8;
+			//draws the current match
+			if(matchN < preMatches.size()){
+				preMatches.get(matchN).drawMatch(g, xs, padding, xsize, ysize, highlightF, "Current Match");
+			}else{
+				if(!(matchN-preMatches.size() >= matches.size())){
+					matches.get(matchN-preMatches.size()).drawMatch(g, xs, padding, xsize, ysize, highlightF, "Current Match");
+				}
+			}
+			padding += padding;
+			padding += ysize;
+			//draws the next match
+			if((matchN+1) < preMatches.size()){
+				preMatches.get(matchN).drawMatch(g, xs, padding, xsize, ysize, highlightF, "Next Match");
+			}else{
+				if(!(matchN+1-preMatches.size() >= matches.size())){
+					matches.get((matchN+1)-preMatches.size()).drawMatch(g, xs, padding, xsize, ysize, highlightF, "Next Match");
+				}
+			}
+			
+		}
 	}
 
 	
@@ -109,31 +336,87 @@ public class BracketDisplay extends Component implements MouseListener{
 	//occurs, all you as a user have to do is handle the signal
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
+		int x = e.getX();
+		int y = e.getY();
+		//System.out.println("Click: "+x+","+y);
+		int minx = 0 + 30;//minx
+		int miny = Toolkit.getDefaultToolkit().getScreenSize().height/8 + 40;//miny
+		int maxx = minx + 160/3;
+		int maxy = miny + 320/3;
+		//the only 2 elements that are clickable are the current and next match
+		//so those are the only 2 that I am going to check
+		
+		//check if mouse was pressed in any of the boxes that can be clicked
+		//in the right x region
+		if(minx < x && maxx > x){
+			//System.out.println("In the correct X region");
+			//check for the y region
+			if(miny < y && maxy > y){
+				//current match
+				System.out.println("Clicked in the current match area");
+				int interval = maxy-miny;
+				System.out.println("" + interval + "   " + y);
+				if(y <= miny + interval/2){
+					if(matchN < preMatches.size()){
+						preMatches.get(matchN).setWinningTeam(0);
+					}else{
+						matches.get(matchN-preMatches.size()).setWinningTeam(0);
+					}
+				}else{
+					if(matchN < preMatches.size()){
+						preMatches.get(matchN).setWinningTeam(1);
+					}else{
+						matches.get(matchN-preMatches.size()).setWinningTeam(1);
+					}
+				}
+				matchN++;
+			}
+			miny += 160;
+			miny += Toolkit.getDefaultToolkit().getScreenSize().height/8;
+			maxy = miny + 320/3;
+			if(miny < y && maxy > y){
+				//next match
+				//System.out.println("YOU CLICKED THE WRONG BUTTON!!!");
+			}
+		}else{
+			//System.out.println("Not in Range "+minx+" to "+maxx);
+		}
 		
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent e) {
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public void mouseExited(MouseEvent e) {
-		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void run() {
+		//repaints the brackets every 20 milliseconds
+		//or roughly 50 fps
+		while(!end){
+			this.repaint();
+			frame++;
+			try {
+				Thread.sleep(20);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 		
 	}
 	
--- BonzAI2017/src/DaneJensenBrackets/Match.java
+++ BonzAI2017/src/DaneJensenBrackets/Match.java
@@ -2,18 +2,46 @@ package DaneJensenBrackets;
 
 import java.awt.*;
 
-public class Match extends Component{
-	private bonzai.Jar[] teams = new bonzai.Jar[2];
+public class Match{
+	protected bonzai.Jar[] teams = new bonzai.Jar[2];
 	private int winningTeam = -1;
-	private int[] scores = {0,0};
 	//font style, font type and font size
-	public static final Font matchFont = new Font("Times New Roman", Font.PLAIN, 12);
+	public static final Font matchFont = new Font("Times New Roman", Font.BOLD, 14);
+	private Match[] depends = new Match[2];
+	
 	
 	public Match(bonzai.Jar team1, bonzai.Jar team2){
 		teams[0] = team1;
 		teams[1] = team2;
 		if(teams[0] == null || teams[1] == null){
 			throw new IllegalArgumentException("A team is null");
+		}
+	}
+	
+	public Match(bonzai.Jar team1){
+		teams[0] = team1;
+	}
+	
+	public Match(Match m1, Match m2){
+		depends[0] = m1;
+		depends[1] = m2;
+	}
+	
+	public Match(Match m1, bonzai.Jar team1){
+		this.teams[0] = team1;
+		this.depends[1] = m1;
+	}
+	
+	public void updateTeams(){
+		if(depends[0] != null && teams[0] == null){
+			if(depends[0].getWinningTeam() != -1){
+				teams[0] = depends[0].teams[depends[0].getWinningTeam()];
+			}
+		}
+		if(depends[1] != null && teams[1] == null){
+			if(depends[1].getWinningTeam() != -1){
+				teams[1] = depends[1].teams[depends[1].getWinningTeam()];
+			}
 		}
 	}
 	
@@ -33,7 +61,8 @@ public class Match extends Component{
 	 * @param ysize		How big it can be in the y direction		
 	 */
 	public void drawMatch(Graphics g, int x, int y, int xsize, int ysize){
-		/*       =======================
+		/*
+		 *        =======================
 		 *       |Start Match          |
 		 *    _  =======================
 		 *   |X| ManticoreAI  (score)   
@@ -43,47 +72,155 @@ public class Match extends Component{
 		 * 
 		 * Rough drawing of what it will look like when all is done,
 		 * the start match is not to scale with the ai names
-		 * 
 		 */
+		updateTeams();
+		if(teams[0] == null && teams[1] == null){
+			drawEmptyMatch(g,x,y,xsize,ysize);
+			return;
+		}
+		g.drawRect(x, y, xsize, ysize);
 		g.setColor(Color.WHITE);
 		//draw the start match button
-		int bY = (ysize * 1) / 5;
-		g.drawRect(xsize/10 + x, y + y, xsize * 9 / 10, bY);//makes a box at the top of the match
+		int bY = (ysize * 1) / 2;
+		//g.fillRect(xsize/10 + x, y, xsize * 9 / 10, bY);//makes a box at the top of the match
 		//auto continue buttons
-		g.drawRect(x, y + bY, xsize/10, bY*2);
-		g.drawRect(x, y + bY*3, xsize/10, bY*2);
+		//g.fillRect(x, y + bY, xsize/10, bY);
+		//g.fillRect(x, y + bY*2, xsize/10, bY);
 		//lines to go under team names
 		g.setColor(Color.BLACK);
-		g.drawLine(x, y + bY, x + xsize, y + bY);
-		g.drawLine(x, y + 3 * bY, x + xsize, y + 3*bY);
+		g.drawLine(x, y + 2 * bY, x + xsize, y + 2 * bY);
+		g.drawLine(x, y + 1 * bY, x + xsize, y + 1*bY);
 		//the input for strings is strange because it takes the bottom left corner
 		//or where the string is supposed to be
 		g.setFont(matchFont);
-		g.drawString(teams[0].name() + "    " + scores[0],x + xsize/10 , bY*3 + y);
-		g.drawString(teams[1].name() + "    " + scores[0],x + xsize/10 , bY*5 + y);
-		g.drawString("Start Match", x + xsize * 3 /10, bY);
+		if(teams[0] != null){
+			if(winningTeam == 0){
+				g.setColor(Color.GREEN);
+				g.fillRect(x, y, xsize, bY);
+				g.setColor(Color.black);
+			}
+			g.drawString(teams[0].name(),x + 5, bY*1 + y - 2);
+		}
+		if(teams[1] != null){
+			if(winningTeam == 1){
+				g.setColor(Color.GREEN);
+				g.fillRect(x, y + bY, xsize, bY);
+				g.setColor(Color.black);
+			}
+			g.drawString(teams[1].name(),x + 5, bY*2 + y - 2);
+		}
+		//g.drawString("Start Match", x + xsize * 3 /10, y+bY-2);
 		
 	}
 	
 	/**
-	 * runs the match 
+	 * Draws the match onto the bracket, all it needs to do is draw the start match
+	 * button the 2 buttons to auto move a team on, the teams names and the scores of
+	 * said teams, or 0 for each if the teams haven't played yet.
 	 * 
-	 * @return the team that won or -1 if their was an error
+	 * The inputed x,y coordinates are expected to be the upper right hand 
+	 * coordinates of the match,
+	 * 
+	 * 
+	 * @param g			The graphics Object for the panel that will be drawing the Match
+	 * @param x			The x value added to the initial position of the Object
+	 * @param x			the y value added to the initial position of the Object
+	 * @param xsize		How big it can be in the x direction
+	 * @param ysize		How big it can be in the y direction		
+	 * @param matchFont The font that will be displayed in the match object
 	 */
-	public int runMatch(){
-		
-		try{
-			//java -jar castles.jar -run [scenarios] <ai/null> <ai/null> <ai/null> <ai/null> <ai/null> <ai/null>
-			String[] args = new String[11]; 
-			
-			
-		}catch(Exception e){
-			//print message to stderr
-			//doesn't crash this thread
-			e.printStackTrace(System.err);
+	public void drawMatch(Graphics g, int x, int y, int xsize, int ysize, Font matchFont, String top){
+		/*
+		 *        =======================
+		 *       |Start Match          |
+		 *    _  =======================
+		 *   |X| ManticoreAI  (score)   
+		 *    =  ======================= 
+		 *   |X| TurtleAI     (score)   
+		 *    =  ======================= 
+		 * 
+		 * Rough drawing of what it will look like when all is done,
+		 * the start match is not to scale with the ai names
+		 */
+		updateTeams();
+		if(teams[0] == null && teams[1] == null){
+			drawEmptyMatch(g,x,y,xsize,ysize);
+			return;
 		}
+		g.drawRect(x, y, xsize, ysize);
+		g.setColor(Color.WHITE);
+		//draw the start match button
+		int bY = (ysize * 1) / 3;
+		g.fillRect(xsize/10 + x, y, xsize * 9 / 10, bY);//makes a box at the top of the match
+		//auto continue buttons
+		g.fillRect(x, y + bY, xsize/10, bY);
+		g.fillRect(x, y + bY*2, xsize/10, bY);
+		//lines to go under team names
+		g.setColor(Color.BLACK);
+		g.drawLine(x, y + 3 * bY, x + xsize, y + 3 * bY);
+		g.drawLine(x, y + 2 * bY, x + xsize, y + 2*bY);
+		//the input for strings is strange because it takes the bottom left corner
+		//or where the string is supposed to be
+		g.setFont(matchFont);
+		if(teams[0] != null){
+			if(winningTeam == 0){
+				g.setColor(Color.GREEN);
+				g.fillRect(xsize/10 + x, y+bY, xsize*9/10, bY);
+				g.setColor(Color.black);
+			}
+			g.drawString(teams[0].name(),x + xsize/10 , bY*2 + y - 2);
+		}
+		if(teams[1] != null){
+			if(winningTeam == 1){
+				g.setColor(Color.GREEN);
+				g.fillRect(xsize/10 + x, y+bY*2, xsize*9/10, bY);
+				g.setColor(Color.black);
+			}
+			g.drawString(teams[1].name(),x + xsize/10 , bY*3 + y - 2);
+		}
+		g.drawString(top, x + xsize * 3 /10, y+bY-2);
 		
+	}
+	
+	public int getWinningTeam(){
 		return winningTeam;
+	}
+	
+	public Match[] getMatchDependencies(){
+		return depends;
+	}
+	
+	public void setMatchDependancy(Match m1){
+		depends[1] = m1;
+	}
+	
+	public void setWinningTeam(int team){
+		winningTeam = team;
+	}
+	
+	public boolean hasTwoTeams(){
+		int i =0;
+		if(teams[0] != null)i+=1;
+		if(teams[1] != null)i+=1;
+		return i == 2;
+	}
+	
+	public static void drawEmptyMatch(Graphics g, int x, int y, int xsize, int ysize){
+		g.drawRect(x, y, xsize, ysize);
+		g.setColor(Color.WHITE);
+		//draw the start match button
+		int bY = (ysize * 1) / 2;
+		//auto continue buttons
+		g.fillRect(x, y, xsize/10, bY);
+		g.fillRect(x, y + bY, xsize/10, bY);
+		//lines to go under team names
+		g.setColor(Color.BLACK);
+		g.drawLine(x, y + 2 * bY, x + xsize, y + 2 * bY);
+		g.drawLine(x, y + 1 * bY, x + xsize, y + 1*bY);
+		//the input for strings is strange because it takes the bottom left corner
+		//or where the string is supposed to be
+		g.setFont(matchFont);
+		//g.drawString("Start Match", x + xsize * 3 /10, y + bY-2);
 	}
 
 }
